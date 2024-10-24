eafb0cd8fa22e238fb4ab3c268c4813b33002085
Valent <valentabinata@gmail.com>
2024-10-24 19:08:36 +0700

    Commiting Project KEMASIN

---
+++ layout/footer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@latest/dist/flowbite.min.css" />
+</head>
+
+<body>
+    <div class="mt-10">
+        <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+
+                <span class="text-sm text-gray-500 dark:text-gray-400"> 
+                    Universitas Pembangunan Nasional "Veteran" Yogyakarta <br>
+                    <a href="https://maps.app.goo.gl/PQzpAZg5t6qJM4zEA" target ="_blank">
+                        Janti, Caturtunggal, Kec. Depok, Kabupaten Sleman <br>
+                        Daerah Istimewa Yogyakarta 55281 <br>
+                    </a>
+                    Indonesia <br>  <br>
+                <a href="beranda.php" class="hover:underline">KEMASIN™</a> © 2024 
+                </span>
+                <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">Tentang Kami</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">Syarat Penggunaan</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">Kebijakan Privasi</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+
+        <script src="https://unpkg.com/flowbite@latest/dist/flowbite.bundle.js"></script>
+    </div>
+</body>
+
+</html>
---
+++ template.php
@@ -0,0 +1,29 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KEMASIN</title>   
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@latest/dist/flowbite.min.css" />
+</head>
+
+<body>
+
+    <?php include "../layout/navbar.php" ?>
+    <div style="margin-top: 5%;">
+
+        <p class="font-bold" style="color: red;"> halo </p>
+
+        <?php include "../layout/footer.php" ?>
+
+    </div>
+
+
+
+    <script src="https://unpkg.com/flowbite@latest/dist/flowbite.bundle.js"></script>
+</body>
+
+</html>
